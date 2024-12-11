3be59fdba4b125d08659fa595c55cd9765724af4
dp259 <122888915+dp259@users.noreply.github.com>
2024-12-11 15:23:23 -0500

    Removed unecessary file and updated sql_tables_python

--- database/sql_tables_python.py
+++ database/sql_tables_python.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Integer, String, Float, MetaData, ForeignKey, Enum, UniqueConstraint
-from sqlalchemy.orm import DeclarativeBase, relationship, registry, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, relationship, registry, Mapped, mapped_column, sessionmaker
 
+engine = create_engine('sqlite:///show_tracker_db.db')
 status_enum = Enum('Planned', 'In-progress', 'Completed', name='status_enum')
 
 metadata = MetaData()
@@ -49,3 +50,34 @@ class User_Trackers(Base):
 
     user = relationship('Users', back_populates = 'user_trackers')
     show = relationship('Shows', back_populates = 'show_trackers')
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind = engine)
+
+try:
+    # Load the datasets
+    df_animes = pd.read_csv('anime-dataset-2023.csv')
+
+    # Select important columns from dataset
+    required_columns = ['anime_id', 'Name', 'English name', 'Episodes', 'Genres']
+    df_animes = df_animes[required_columns]
+
+    df_animes['Episodes'] = df_animes['Episodes'].astype(str)
+    df_animes = df_animes[~df_animes['Episodes'].str.contains('unknown', case = False)]  # Filter out 'UNKNOWN' values\
+
+    df_animes['anime_id'] = pd.to_numeric(df_animes['anime_id'], errors='coerce').fillna(0).astype(int)
+    df_animes['Episodes'] = pd.to_numeric(df_animes['Episodes'], errors='coerce').fillna(0).astype(int)
+
+    #Add datasets to respective table
+    animes = [
+        Animes(anime_id = row['anime_id'], name = row['Name'], english_name = row['English name'],
+            episodes = row['Episodes'], genres = row['Genres'])
+        for _, row in df_animes.iterrows()
+    ]
+
+    with Session() as session:
+        session.bulk_save_objects(animes)
+        session.commit()
+except Exception as e:
+    print(f"Error, {e}, was encountered")
